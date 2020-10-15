a74ef158c8dcbc266f95c4282e99751a9ae201c7
Ganick Bazoungoula <ganickbazoungoula@Ganicks-MacBook-Air.local>
2020-10-15 15:27:28 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyttsx3
+import PyPDF2
+book = open('student-code-of-conduct-au.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
+speaker = pyttsx3.init()
+for num in range(4, pages):
+    page = pdfReader.getPage(4)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
